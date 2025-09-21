8111caf95169f38a1ffe619926692c8501b6f639
VietLong <vietlong.eee@gmail.com>
2025-09-21 23:52:40 +0700

    use environment variables

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,2 +1,17 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
-export const MEDIA_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = (() => {
+    if (import.meta.env.VITE_ENVIRONMENT === 'development') {
+        return import.meta.env.VITE_API_BASE_URL_DEV;
+    } else {
+        return import.meta.env.VITE_API_BASE_URL_PROD;
+    }
+})();
+
+const MEDIA_BASE_URL = (() => {
+    if (import.meta.env.VITE_FILE_UPLOAD_PROVIDER === 'local') {
+        return import.meta.env.VITE_MEDIA_BASE_URL_LOCAL;
+    } else {
+        return '';
+    }
+})();
+
+export { API_BASE_URL, MEDIA_BASE_URL };
